4cd4111fbce77a0fd2f78067292b14af3bb7627c
Troy Caro <65050284+iamtroy412@users.noreply.github.com>
2022-05-06 16:07:59 -0400

    Adds redner function

--- src/main.rs
+++ src/main.rs
@@ -111,6 +111,30 @@ fn test_pixel_to_point() {
     );
 }
 
+// Render a rectanlge of the Mandelbrot set into a buffer of pixels.
+//
+// The 'bounds' argument gives the width and height of the buffer 'pixels',
+// which holds one grayscale pixel per byte. The 'upper_left' and 'lower_right'
+// arguments specify points on the complex plane corresponding to the upper.
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
+
 fn main() {
     println!("...still working on this");
 }
