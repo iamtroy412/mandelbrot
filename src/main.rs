c26b2df03f5120c6628b6d5209d40613cb75ca54
Troy Caro <65050284+iamtroy412@users.noreply.github.com>
2022-05-06 16:18:30 -0400

    Adds write_image function and deps

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use image::png::PNGEncoder;
+use image::ColorType;
 use num::Complex;
+use std::fs::File;
 use std::str::FromStr;
 
 // Try to determine if 'c' is in the Mandelbrot set, using at most 'limit'
@@ -133,6 +136,20 @@ fn render(
             };
         }
     }
+}
+
+// Write the buffer 'pixels', whose dimentions are given by 'bounds', to the
+// file named 'filename'.
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+    Ok(())
 }
 
 fn main() {
